a6915dc757d90a5ce0054a66df6cbc580d57dfe0
Lalitha G <guthikondalalitha@gmail.com>
2022-11-30 09:13:22 +1100

    Deate task added

--- second-page.js
+++ second-page.js
@@ -4,31 +4,40 @@ import validateTaskForm from "./inputValidation.js"
 
 let taskForm = document.getElementById('taskForm')
 let tasksList = []
-window.addEventListener('load',()=>{
+window.addEventListener('load', () => {
   displayTasks()
-//  let TaskItems= document.getElementById("list-items")
-  
-  let tasks=document.querySelectorAll('.taskId')
-  tasks.forEach(task=>{
-    let id =task.getAttribute('id')
-   
-    task.addEventListener('click',()=>{
-    console.log('click')
-    console.log(id)
-    TaskManager.updateStatus(id)
-    task.style.backgroundColor='green'
-    task.innerHTML='Done'
+  //  let TaskItems= document.getElementById("list-items")
 
-    })
-  })
-  console.log(tasks)
-  function statusDone(){
-   console.log('status done')
-   console.log('click')
+  addButtonEventListener('.taskId');
+  addButtonEventListener('.taskId1');
 
- }
+  // function statusDone() {
+  //   console.log('status done')
+  //   console.log('click')
+  // }
+
 })
 
+function addButtonEventListener(buttonClass) {
+  let tasks = document.querySelectorAll(buttonClass);
+
+  console.log("Tasks: " + JSON.stringify(tasks));
+
+  tasks.forEach(task => {
+    let id = task.getAttribute('id');
+
+    task.addEventListener('click', (event) => {
+      console.log('click: ' + JSON.stringify(event));
+      console.log("Id: " + id);
+      if (buttonClass == '.taskId')
+        TaskManager.updateStatus(id);
+      else if(buttonClass == '.taskId1')
+        TaskManager.deleteTask(id);
+    });
+  });
+
+  console.log(tasks);
+}
 
 const openForm = document.getElementById("addTask");
 openForm.addEventListener('click', addTaskForm)
@@ -46,7 +55,7 @@ function closeTaskForm() {
   document.getElementById('taskFormData').style.display = "none";
 }
 
-  const btnSubmit=document.getElementById('btnSubmit')
+const btnSubmit = document.getElementById('btnSubmit')
 btnSubmit.addEventListener('click', (event) => {
 
   event.preventDefault();
@@ -55,7 +64,7 @@ btnSubmit.addEventListener('click', (event) => {
     console.log('validate fails');
     return
   }
- 
+
   const task = new TaskManager()
   task._taskName = taskForm['taskName'].value
   task._desc = taskForm['taskDes'].value
@@ -63,11 +72,11 @@ btnSubmit.addEventListener('click', (event) => {
   task._assignTo = taskForm['assignTo'].value
   task._dueDate = taskForm['dueDate'].value
 
-  
-   console.log(task)
-   TaskManager.addTask(task)
- 
-   // closeForm();
+
+  console.log(task)
+  TaskManager.addTask(task)
+
+  // closeForm();
   window.location.href = '/second-page.html'
 
 })
@@ -87,61 +96,50 @@ function myFunction() {
   document.getElementById("todaysDate").style.color = "white";
 }
 
-   const taskManager = new TaskManager();
-    let allTasks = TaskManager.getTaskList();
-  // console.log("TASKS LISTS:::" + taskManager.getTaskList());
-   let taskFilter=document.getElementById('taskFilter')
-   console.log(`task filter ${taskFilter.value}`)
-   taskFilter.addEventListener('change',()=>{
-       console.log('in select')
-       console.log(taskFilter.value)
-       allTasks =TaskManager.taskListByStatus(taskFilter.value);
-       console.log(allTasks)
-       document.getElementById("list-items").innerHTML=''
-        displayTasks()
-   })
-  function addItem(item) {
-    const itemHTML = '<div class="card" style="width: 100%;">\n' +
-      '        <div class="card-body">\n' +
-      '        <h5 class="card-title taskNameJs text-primary" >' + item._taskName + '</h5>\n' +
-      '        <p class="card-text font-weight-bold assigned-to">' + 'Assigned to: ' + item._assignTo + '</p>\n' +
-      '        <p class="card-text font-weight-bold status">' + 'Status: ' + item._status + '</p>\n' +
-      '        <p class="card-text des">' + 'Description: ' + item._desc + '</p>\n' +
-      '        <p class="card-text des">' + 'id: ' + item._id + '</p>\n' +
-      '        <p class="card-text font-weight-bold dueDate">' + 'Due Date: ' + new Date(item._dueDate).toLocaleDateString("en-Uk") + '</p>\n' +
-      '        <a href="#" class="btn btn-primary" id="deleteTaskInfo">Delete</a>\n' +
-      '        <a href="#" class=" taskId btn btn-primary" id='+item._id+' >Mark Done</a>\n' +
-      '    </div>\n' +
-      '</div>\n' +
-      '<br/>';
-    const itemsContainer = document.getElementById("list-items");
-    itemsContainer.innerHTML += itemHTML;
-    // const deleteId = document.getElementById("deleteTaskInfo");
-    // deleteId.addEventListener('click',deleteTask(item._id));
-  }
-  function displayTasks(){
+const taskManager = new TaskManager();
+let allTasks = TaskManager.getTaskList();
+// console.log("TASKS LISTS:::" + taskManager.getTaskList());
+let taskFilter = document.getElementById('taskFilter')
+console.log(`task filter ${taskFilter.value}`)
+taskFilter.addEventListener('change', () => {
+  console.log('in select')
+  console.log(taskFilter.value)
+  allTasks = TaskManager.taskListByStatus(taskFilter.value);
+  console.log(allTasks)
+  document.getElementById("list-items").innerHTML = ''
+  displayTasks()
+})
 
-    allTasks.map(task => {
-      addItem(task);
-    })
-  }
+function addItem(item) {
+  let status = (item._status == 'done') ? true : false;
 
+  console.log("Status: " + status + " ," + item._status);
+  let itemHTML = '<div class="card" style="width: 100%;">\n' +
+    '        <div class="card-body">\n' +
+    '        <h5 class="card-title taskNameJs text-primary" >' + item._taskName + '</h5>\n' +
+    '        <p class="card-text font-weight-bold assigned-to">' + 'Assigned to: ' + item._assignTo + '</p>\n' +
+    '        <p class="card-text font-weight-bold status">' + 'Status: ' + item._status + '</p>\n' +
+    '        <p class="card-text des">' + 'Description: ' + item._desc + '</p>\n' +
+    '        <p class="card-text des">' + 'id: ' + item._id + '</p>\n' +
+    '        <p class="card-text font-weight-bold dueDate">' + 'Due Date: ' + new Date(item._dueDate).toLocaleDateString("en-Uk") + '</p>\n' +
+    '        <a href="#" class="taskId1 btn btn-primary" id=' + item._id + '>Delete</a>\n' +
+    ((!status) ?
+      '        <a href="#" class="taskId btn btn-primary  " id=' + item._id + '>' + "Mark Done" + '</a>\n'
+      // : '');
+      :
+      ' <a href="#" class="taskId btn btn-primary done" id=' + item._id + '>' + "Done" + '</a>\n'); +
+  '    </div>\n' +
+  '</div>\n' +
+  '<br/>';
+  const itemsContainer = document.getElementById("list-items");
+  itemsContainer.innerHTML += itemHTML;
 
-// function deleteTask(id) {
+}
 
-//   console.log("Delete Task");
-//   const taskManager = new TaskManager();
-//   let allTasks = taskManager.getTaskList();
+function displayTasks() {
 
-
-
-//   const index = allTasks.findIndex(task => {
-//     task._id === id
-//   });
-
-//   console.log("Index::::" + index)
-//   allTasks = allTasks.splice(index, 1);
-//   console.log(allTasks);
-
-  //window.localStorage.setItem('Tasks',JSON.stringify(allTasks));
-//}
+  allTasks.map(task => {
+    console.log("Display Tasks: " + JSON.stringify(task));
+    addItem(task);
+  })
+}

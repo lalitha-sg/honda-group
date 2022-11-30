4ca34c8b53c342b95f7c82f261a12e4f792db610
kanwar Dhillon <kdhillon469@gmail.com>
2022-12-01 10:46:08 +1100

    fix validation & clear span onfocus

--- inputValidation.js
+++ inputValidation.js
@@ -1,21 +1,39 @@
+
+   window.addEventListener('load',()=>{
+    ///function to clear span on focus event
+     function clearError(spanId){
+       document.getElementById(spanId).innerHTML=''
+      }
+      const inputElements=['taskName','taskDes','assignTo','dueDate'];
+    console.log('in load')
+        inputElements.forEach(element=>{
+          document.getElementById(element).onfocus= ()=> clearError(`${element}Span`)
+        })
+   });
 function validateTaskForm() {
+  let validation=true;
+
   let tName = document.forms["taskForm"]["taskName"].value;
-  let taskNameMsg = document.getElementById('errMsg')
   if (tName == "" || tName.length < 8) {
     console.log(tName.length)
     // alert("Task name must be filled out and longer than 8 characters!");
-    taskNameMsg.innerHTML = 'Task name must be filled out and longer than 8 characters!';
-    return false;
+    document.getElementById('taskNameSpan').innerHTML = 'Task name must be filled out and longer than 8 characters!';
+    validation=false
+    // return false;
   }
   let tDes = document.forms["taskForm"]["taskDes"].value;
   if (tDes == "" || tDes.length < 15) {
-    alert("Description must be filled out and longer than 15 characters!");
-    return false;
+    // alert("Description must be filled out and longer than 15 characters!");
+    document.getElementById('taskDesSpan').innerHTML = 'Task Des must be filled out and longer than 15 characters!';
+    validation=false
+    // return false;
   }
   let assign = document.forms["taskForm"]["assignTo"].value;
   if (assign == "") {
-    alert("Task must be assigned to someone");
-    return false;
+    // alert("Task must be assigned to someone");
+    document.getElementById('assignToSpan').innerHTML = 'Task  must be assigned!';
+    validation=false
+    // return false;
   }
   let dueDate = document.forms["taskForm"]["dueDate"].value;
   let today = new Date();
@@ -25,8 +43,11 @@ function validateTaskForm() {
   let minDate = `${yearToday}-${monthToday}-${dateToday}`;
   //dueDate.min = minDate;
   if (minDate > dueDate) {
-    alert("Date must be in the future!")
-    return false;
-  } else return true;
+    // alert("Date must be in the future!")
+    document.getElementById('dueDateSpan').innerHTML = 'Due Date must be filled out in future !';
+    // return false;
+    validation=false
+  }
+    return validation;
 }
 export default validateTaskForm
